760164bf06b201d80455e8d055a7f84ee20cd272
Julian Swerdlin <julianswer36@gmail.com>
2022-01-01 18:33:22 +0100

    new files

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Container } from 'semantic-ui-react';
+import Navbar from 'src/components/Navbar';
 
 export default function Layout(props) {
     return (
         <div>
-            <h1>Navbar</h1>
-            {props.children}
+            <Navbar />
+            <Container style={{paddingTop: '2rem'}}>
+                {props.children}
+            </Container>
         </div>
     )
 }

05246920492d61e339605a3fd4939f96ea1c547b
Julian Swerdlin <julianswer36@gmail.com>
2022-01-01 03:10:40 +0100

    new files

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,7 +13,10 @@ export default function TaskList({tasks} : Props) {
                 <Card key={task.id}>
                     <Card.Content>
                         <Card.Header>{task.title}</Card.Header>
-                        <Card.Meta>{task.description}</Card.Meta>
+                        {task.created_on && (
+                            <Card.Meta>{new Date(task.created_on).toLocaleDateString()}</Card.Meta>
+                        )}
+                        <Card.Description>{task.description}</Card.Description>
                     </Card.Content>
                 </Card>
             ))}
